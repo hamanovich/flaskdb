7871377cda3343f7c8b9c1fb1a580ef88581e498
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2020-01-12 00:40:40 +0300

    Flask, sqlalchemy, migration db

---
+++ .flaskenv
@@ -0,0 +1,1 @@
+FLASK_ENV=flaskdb.py
---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from flask import redirect, render_template, flash, url_for
+
+from app import app
+from app.forms import LoginForm
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Siarhei'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
