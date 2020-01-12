090a6c1edfcc3089f140c4ef9a0a1d55be0e2223
Siarhei Hamanovich <siarhei_hamanovich@epam.com>
2020-01-12 17:37:14 +0300

    404

--- app/routes.py
+++ app/routes.py
@@ -62,3 +62,7 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+   return render_template('404.html', title = '404'), 404
